d1714eb8900eee59d2351f2372731beaa59701e8
Soebb <87156166+Soebb@users.noreply.github.com>
2021-07-28 03:10:42 +0430

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ async def start(bot, update):
 @bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
 async def link_handler(c, m):
     headers = {
-        "User-agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
+        "User-agent": 'Mozilla/5.0 (Linux; Android 4.4.2; Hol-U19) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36'}
     URL = 'https://9xbuddy.com/process?url=https://twitter.com/dreamforce/status/922920747709820928?lang=fa'
     page = requests.get(URL, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
