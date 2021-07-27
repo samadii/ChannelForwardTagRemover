ee58fe47793d31c39ba7bc7fd0367bf68003ed6a
Soebb <87156166+Soebb@users.noreply.github.com>
2021-07-28 02:51:29 +0430

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ async def start(bot, update):
 async def link_handler(c, m):
     headers = {
         "User-agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
-    URL = 'https://9xbud.com/https://twitter.com/dreamforce/status/922920747709820928?lang=fa'
+    URL = 'https://9xbuddy.com/process?url=https://twitter.com/dreamforce/status/922920747709820928?lang=fa'
     page = requests.get(URL, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
     links = soup.findAll('a')
