fda3365374813bec61c111cbf4d6879681f0845f
Soebb <87156166+Soebb@users.noreply.github.com>
2021-07-29 19:53:40 +0430

    Update main.py

--- main.py
+++ main.py
@@ -40,14 +40,14 @@ async def link_handler(c, m):
     headers = {
         "User-agent": 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36'}
     URL = 'https://9xbuddy.com/process?url=https://twitter.com/dreamforce/status/922920747709820928?lang=fa'
-    page = await requests.get(URL, headers=headers)
+    r = requests.get(URL)
+    r.cookies['example_cookie_name']
+    page = requests.get(URL, headers=headers)
     time.sleep(8)
     soup = BeautifulSoup(page.content, 'html.parser')
     time.sleep(8)
     links = soup.findAll('a')
-    for link in links:
-        link_url = link["href"]
-        await m.reply(f"{link_url}")
+    await m.reply(f"{r.text}")
 
 
 bot.run()
