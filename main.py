a1727ce1ca6bddf9d2965b11e7f45282831abc5f
Soebb <87156166+Soebb@users.noreply.github.com>
2021-07-30 04:30:06 +0430

    Update main.py

--- main.py
+++ main.py
@@ -40,11 +40,11 @@ async def start(bot, update):
 @bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
 async def link_handler(c, m):
     headers = {
-        "User-agent": 'Mozilla/5.0 (Linux; Android 4.4.2; Hol-U19) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36'}
+        "User-agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
     URL = f'{m.text}'
     r = scraper.get(URL, headers=headers)
     soup = BeautifulSoup(r.content, 'html.parser')
-    links = soup.findAll('p')
+    links = soup.findAll('a')
     for link in links:
         result = link['href']
         await m.reply(f"{result}")
